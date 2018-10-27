4664661a0f3c97f17bdfb249473a568d80818d09
Karuppiah Natarajan <karuppiah7890@gmail.com>
2018-10-27 21:36:07 +0530

    Add feature to create multiple tmuxp configurations for multiple gke kubernetes clusters

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/karuppiah7890/kube-tmuxp-config-gen
 
 require (
 	github.com/ghodss/yaml v1.0.0
+	github.com/go-bindata/go-bindata v1.0.0 // indirect
 	golang.org/x/net v0.0.0-20181023162649-9b4f9f5ad519 // indirect
 	google.golang.org/genproto v0.0.0-20181026194446-8b5d7a19e2d9 // indirect
 	google.golang.org/grpc v1.16.0 // indirect
---
+++ tmuxpconfig/tmuxpconfig.go
@@ -0,0 +1,39 @@
+package tmuxpconfig
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"text/template"
+
+	templates "github.com/karuppiah7890/kube-tmuxp-config-gen/templates"
+	utils "github.com/karuppiah7890/kube-tmuxp-config-gen/utils"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+// CreateTmuxpConfig creates tmuxp config
+func CreateTmuxpConfig(kubeContext string, tmuxSessionName string) {
+	fmt.Println("Creating tmuxp configuration")
+	templateByteData, err := templates.Asset("templates/tmuxp-config.yaml")
+	check(err)
+	tmuxpconfigTemplate, err := template.New("tmuxpconfig").Parse(string(templateByteData))
+	check(err)
+
+	templateValues := make(map[string]interface{})
+	kubeConfigFile := path.Join(utils.GetHomeDir(), ".kube", "configs", kubeContext)
+	templateValues["KubeConfig"] = kubeConfigFile
+	templateValues["SessionName"] = tmuxSessionName
+
+	tmuxpConfigFilePath := path.Join(utils.GetHomeDir(), ".tmuxp", fmt.Sprintf("%v.yaml", kubeContext))
+	tmuxpConfigFile, err := os.Create(tmuxpConfigFilePath)
+	check(err)
+	defer tmuxpConfigFile.Close()
+	err = tmuxpconfigTemplate.ExecuteTemplate(tmuxpConfigFile, "tmuxpconfig", templateValues)
+	check(err)
+	fmt.Println("Created tmuxp configuration")
+}
